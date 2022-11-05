8ddf9d473a11dff127607a9a0a09e30ba62d5386
Humberto Cruz Sanabria <mc.humbertocruz@outlook.com>
2022-11-04 19:50:22 -0600

    fixing

--- changeHP.js
+++ changeHP.js
@@ -4,15 +4,12 @@ const file = require(fileName);
 
 //file.key = "new value";
 console.log(file.homepage);
-file.homepage = "https://humberto-site.netlify.app/";
+file.homepage = "https://humberto-site.netlify.app";
 console.log(file.homepage);
 console.log(file.name);
-if (file.hasOwnProperty("homepage")) {
-  file.homepage = "humberto-site.netlify.app";
-}
 
 fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
   if (err) return console.log(err);
-  //console.log(JSON.stringify(file));
+  console.log(JSON.stringify(file));
   //console.log("writing to " + fileName);
 });
